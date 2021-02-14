08a31c4bf6916b1e8bbefd5993c789658ff8e630
David Kerkeslager <kerkeslager@gmail.com>
2021-02-14 18:23:27 -0500

    Add a database schema

---
+++ schema.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import declarative_base
+import sqlalchemy as sa
+
+Base = declarative_base()
+
+class Wish(Base):
+    __tablename__ == 'wishes'
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    url = sa.Column(sa.UnicodeText, nullable=False)
+
+metadata = Base.metadata
