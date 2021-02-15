ec575fb2ced88f685a77ba6344ff7727ef90dd21
David Kerkeslager <kerkeslager@gmail.com>
2021-02-14 19:02:33 -0500

    Get alembic to generate the schema migrations

--- storage/schema.py
+++ storage/schema.py
@@ -4,7 +4,7 @@ import sqlalchemy as sa
 Base = declarative_base()
 
 class Wish(Base):
-    __tablename__ == 'wishes'
+    __tablename__ = 'wishes'
 
     id = sa.Column(sa.Integer, primary_key=True)
     url = sa.Column(sa.UnicodeText, nullable=False)
